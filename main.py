8c9995f7eae25a9b6a9b115a1aa75cad38525ff0
nyshanbaev <bajtikovskidmitrij@gmail.com>
2023-01-26 12:13:03 +0600

    newtest

---
+++ main.py
@@ -0,0 +1,20 @@
+# file = open('test1.txt', 'a') 
+# # file.write('hello')
+# # file.write('world')
+# file.write('Hello')
+# file.close()
+
+# # JSON
+# # serialization(python->json) -> dump, dumps
+# # deserialization(json->python) -> load, loads
+# import json
+# d = {'John': True, 'age': 2, 'name': None}
+# json_obj = json.dumps(d)
+# print(json_obj)
+# python_obj = json.loads(json_obj)
+# print(python_obj)
+
+# with open('data.json', 'r') as f:
+#     print(json.load(f))
+
+
